15e5b6ad58d690452930e64ac9b0898fd737e575
shturmovic078 <agolubev1@usnycpww4255.global.publicisgroupe.net>
2017-11-08 17:53:45 -0500

    Updated the preference center
    This test is using: TestNG, BrowserStack, aShot, with Extended Reports

---
+++ automation/src/main/java/brand/automation/App.java
@@ -0,0 +1,13 @@
+package brand.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
